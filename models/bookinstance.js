4fdda4194ebbba5d41d9500b43b8e28cd8e7cb09
Jimmy Quadros <jimmyquadros@hotmail.com>
2023-03-28 17:06:47 -0400

    first version of application moved into github

---
+++ models/bookinstance.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+
+const BookInstanceSchema = new Schema({
+    book: { type: Schema.Types.ObjectId, ref: 'Book', requried: true },
+    imprint: { type: String, required: true },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
+        default: 'Maintenance',
+    },
+    due_back: { type: Date, default: Date.now },
+});
+
+// Virtual for bookinstance url
+BookInstanceSchema.virtual('url').get(function() {
+    return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+BookInstanceSchema.virtual('due_back_yyyymmdd').get(function() {
+    return DateTime.fromJSDate(this.due_back).toFormat('yyyy-MM-dd');
+})
+
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
